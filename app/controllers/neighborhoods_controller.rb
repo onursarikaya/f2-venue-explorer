4d596c0890eb62fbbe9add4984eb9a4a7e00ed8b
MEHMET AYDOĞDU <mhmt.dp@gmail.com>
2015-09-04 18:13:46 +0300

    added show action

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,9 +1,15 @@
 class NeighborhoodsController < ApplicationController
   def index
-    @area = Neighborhood.find(params[:neighborhood_id])
+    @area = Neighborhood.all
+    #find(params[:neighborhood_id])
     filter = params[:filter]
     foursquare = Foursquare.new(@area, filter)
     @establishments = foursquare.establish_establishments
   end
+
+  def show
+  @area = Neighborhood.find(params[:id])
+  @filters = ['Hotel']
+end
 
 end
